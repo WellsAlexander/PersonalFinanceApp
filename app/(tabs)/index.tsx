b001ceddf4304cc13c54ade995dacd7d34c00166
WellsAlexander <amwells2003@gmail.com>
2025-03-07 15:18:45 +1300

    added sign-out button for testing purposes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  SafeAreaView,
+} from "react-native";
+import { auth } from "../../firebaseConfig";
+import { router } from "expo-router";
+import { getAuth } from "firebase/auth";
 
-const index = () => {
+export default function SignOutScreen() {
+  getAuth().onAuthStateChanged((user) => {
+    if (!user) {
+      router.replace("/resetToRoot");
+    }
+  });
+
   return (
-    <View>
-      <Text>tabs index</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => auth.signOut()}>
+          <Text style={styles.text}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-};
+}
 
-export default index;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#000",
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    color: "#fff",
+  },
+});
